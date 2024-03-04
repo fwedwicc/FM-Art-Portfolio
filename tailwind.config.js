d6530198f1fa5f310d9c1bef06b41c95df6cae40
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-03-04 21:45:12 +0800

    Change the main file to daisyui tester and tried some components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('preline/plugin'),
     require("daisyui"),
   ],
 }
